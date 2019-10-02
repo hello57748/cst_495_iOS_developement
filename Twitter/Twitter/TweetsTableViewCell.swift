2f724843b3bdc080298d291a83d889f3e56ca0d5
hello57748 <sampeters@csumb.edu>
2019-10-02 15:05:24 -0700

    Twitter finished

---
+++ Twitter/Twitter/TweetsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetsTableViewCell.swift
+//  Twitter
+//
+//  Created by Samuel Peters on 10/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
